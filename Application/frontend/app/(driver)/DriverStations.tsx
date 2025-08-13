0a23423ac03851c7279dad12a5a9ce3428c16647
Aaron Señar <195650917+IT-Abby@users.noreply.github.com>
2025-08-13 23:59:29 +0800

    Update DriverStations.tsx

--- Application/frontend/app/(driver)/DriverStations.tsx
+++ Application/frontend/app/(driver)/DriverStations.tsx
@@ -1,29 +1,74 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
 import "@fontsource/poppins";
+import Mapbox from "@rnmapbox/maps";
 import BackButton from "@/components/Backbutton";
 
+const { width, height } = Dimensions.get("window");
+
+Mapbox.setAccessToken(
+  "pk.eyJ1IjoiZXJpY3RhbjMzMyIsImEiOiJjbWU4NTVsamswOWNuMmpwd29lZmx1OTNwIn0.1rtunFwJarUUNmyOKSdSYQ"
+);
+
 export default class DriverStations extends Component {
+  state = {
+    mapReady: false,
+  };
+
+  handleMapReady = () => {
+    this.setState({ mapReady: true });
+  };
+
   render() {
     return (
-      <View style={rentStyles.container}>
-        <BackButton />
-        <Text style={rentStyles.header}> Stations </Text>
+      <View style={styles.container}>
+        <Mapbox.MapView
+          style={styles.map}
+          styleURL={Mapbox.StyleURL.Street}
+          onDidFinishLoadingMap={this.handleMapReady}
+        >
+          {this.state.mapReady && (
+            <Mapbox.Camera
+              zoomLevel={14}
+              centerCoordinate={[123.186389, 13.624444]}
+            />
+          )}
+        </Mapbox.MapView>
+
+        <View style={styles.topBar}>
+          <BackButton />
+          <Text style={styles.title}>Stations</Text>
+          <View style={{ width: 50 }} />
+        </View>
       </View>
     );
   }
 }
 
-const rentStyles = StyleSheet.create({
-  header: {
-    fontWeight: "bold",
-    fontSize: 25,
-    marginLeft: 75,
-    color: "#073051",
-    paddingTop: 50,
-  },
+const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: width,
+    height: height,
+  },
+  topBar: {
+    marginTop: 10,
+    paddingHorizontal: 13,
     flexDirection: "row",
     alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 25,
+    marginLeft: 1,
+    color: "#073051",
+    paddingTop: 50,
   },
 });
